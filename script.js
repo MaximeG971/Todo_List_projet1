6169a6b4c16f8c0b64a24153aefa9c73af3924ba
MAXIME GAEL <maximejordan.gael@gmail.com>
2023-09-30 20:04:03 +0200

    ajout du bouton cliquable light mode / dark mode avec changement de theme

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ btnClose.addEventListener('click', closeModal);
 function closeModal(){
     overlay.style.display = 'none'
 };
+
+const changeThemeBtn = document.querySelector('.changeTheme');
+let toggleTheme = 0;
+changeThemeBtn.addEventListener('click', () => {
+    if(toggleTheme === 0) {
+        document.documentElement.style.setProperty('--ecriture','#262626');
+        document.documentElement.style.setProperty('--background','#fff8f0');
+        toggleTheme++;
+    } else {
+        document.documentElement.style.setProperty('--ecriture','#fff8f0');
+        document.documentElement.style.setProperty('--background','#262626');
+        toggleTheme--;
+    }
+})
